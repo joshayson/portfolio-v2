493b5105a3fbfa924565a746954c05c0b569ad7b
joshayson <njqayson@gmail.com>
2025-04-23 10:51:20 -0400

    added new font family, added js page w/ logic for heading and link mouse highlighting + keyboard nav, added project page, updated styling

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("keydown", (e) => {
+    const key = e.key.toLowerCase();
+  
+    const pageMap = {
+      h: "index.html",
+      p: "projects.html",
+      l: "index.html#links" 
+    };
+  
+    const targetPage = pageMap[key];
+  
+    if (targetPage) {
+      window.location.href = targetPage;
+    }
+  });
+
+// hover effect for headings 
+const descriptionHeading = document.querySelectorAll(".description-container h3");
+
+descriptionHeading.forEach((heading) => {
+  heading.addEventListener("mouseenter", () => {
+    heading.style.color = "rgb(255, 64, 0)";
+  });
+
+  heading.addEventListener("mouseleave", () => {
+    heading.style.color = "white";
+  });
+});
+
+// hover effect for links
+const links = document.querySelectorAll("li");
+
+
+links.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+        link.style.color = "rgb(255, 64, 0)";
+        console.log('selected')
+    });
+
+    link.addEventListener("mouseleave", () => {
+        link.style.color = "white";
+    });
+})
