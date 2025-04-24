94a4bf358a86c6feb56871ab1d6cf4c0661e0516
joshayson <njqayson@gmail.com>
2025-04-24 10:30:44 -0400

    deleted projects page, updated nav links, updated js hover to css hover, added projects (format, styling, link), added updated descriptions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", (e) => {
   
     const pageMap = {
       h: "index.html",
-      p: "projects.html",
+      p: "index.html#projects",
       l: "index.html#links" 
     };
   
@@ -13,46 +13,3 @@ document.addEventListener("keydown", (e) => {
       window.location.href = targetPage;
     }
   });
-
-// hover effect for headings 
-const descriptionHeading = document.querySelectorAll(".description-container h3");
-
-descriptionHeading.forEach((heading) => {
-  heading.addEventListener("mouseenter", () => {
-    heading.style.color = "rgb(255, 64, 0)";
-  });
-
-  heading.addEventListener("mouseleave", () => {
-    heading.style.color = "white";
-  });
-});
-
-
-// hover effect for links
-const links = document.querySelectorAll("li");
-
-links.forEach((link) => {
-    link.addEventListener("mouseenter", () => {
-        link.style.color = "rgb(255, 64, 0)";
-        console.log('selected')
-    });
-
-    link.addEventListener("mouseleave", () => {
-        link.style.color = "white";
-    });
-})
-
-
-// hover effect for all projects link
-
-const allProjects = document.querySelector("#all-projects")
-
-allProjects.addEventListener("mouseenter", () => {
-    allProjects.style.color = "rgb(255, 64, 0)";
-    allProjects.style.textDecoration = "underline";
-});
-
-allProjects.addEventListener("mouseleave", () => {
-    allProjects.style.color = "white";
-    allProjects.style.textDecoration = "none";
-});
